0e188e18a69039e9e2a960f184ee500923717e6e
JL <52089352+TheJohnsonLai@users.noreply.github.com>
2021-03-23 00:22:06 -0400

    Integration of Instructor Authorized View sample

--- a3/app.py
+++ a3/app.py
@@ -8,8 +8,6 @@ DATABASE = './assignment3.db'
 
 # the function get_db is from
 # https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
-
-
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -19,8 +17,6 @@ def get_db():
 
 # the function query_db is from
 # https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
-
-
 def query_db(query, args=(), one=False):  # 1=F means select all entries
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
@@ -29,11 +25,10 @@ def query_db(query, args=(), one=False):  # 1=F means select all entries
 
 # the function make_dicts is from
 # https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
-
-
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
                 for idx, value in enumerate(row))
+
 
 # ---------------------------------------------- Flask ------------------------------------------------
 
@@ -41,14 +36,11 @@ app = Flask(__name__)
 
 # ---------------------------------------------- Authentication ---------------------------------------
 
-#app.secret_key = "thesecretkey"
-#This stuff is yet to be decided - how to implement authentication. Cannot use SQLalchemy.
-#class User():
-#    id = 0
-#    username = ''
-#   password = ''
-#   userType = "None"
-#   verified = False
+app.secret_key = "thesecretkey"
+# This stuff is yet to be decided - how to implement authentication. Cannot use SQLalchemy.
+# session['VARNAME'] = VALUE
+# session['user_type'] = instructor
+# session['username'] = instructor1
 
 # the function close_connection is from
 # https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
@@ -64,86 +56,93 @@ def close_connection(exception):
 @app.route('/sviewgrades.html', methods=['GET', 'POST'])
 def student_view_grades():
     if request.method == 'GET':
-        db=get_db()
+        db = get_db()
         # Each row from the table is placed in dictionary form
         db.row_factory = make_dicts
-        utorid = 1 #request.args.get('utorid')#'utorid'can be other things depend on what login page returns 
-        student_name = query_db('select * from STUDENT where UTORID = ?', [utorid], one=True)
+        # request.args.get('utorid')#'utorid'can be other things depend on what login page returns
+        utorid = 1
+        student_name = query_db(
+            'select * from STUDENT where UTORID = ?', [utorid], one=True)
         name = student_name['FNAME'] + ' ' + student_name['LNAME']
-        #If no such student to be implemented
-        
-        
-        # Inside DB are some tables.
-        student_grades = query_db('select * from GRADES where UTORID = ?', [utorid], one=True)
-        db.close()
-        #return student_grades.__str__()
-        return render_template('sviewgrades.html', grade = student_grades, name = name)
+        # If no such student to be implemented
 
+        # Inside DB are some tables.
+        student_grades = query_db(
+            'select * from GRADES where UTORID = ?', [utorid], one=True)
+        db.close()
+        # return student_grades.__str__()
+        return render_template('sviewgrades.html', grade=student_grades, name=name)
 
 # To be implemented (Student grades - view. Currently lists students, accessed in HTML by {% for item in usersH %})
 # Can change grades.
 @app.route('/iviewgrades.html', methods=['GET', 'POST'])
 def instructor_view_grades():
+    if session['user_type'] != "instructor"        
+        return redirect(redirect_url())
     if request.method == 'GET':
         db = get_db()
         # Each row from the table is placed in dictionary form
         db.row_factory = make_dicts
-
         students = []
         # Inside DB are some tables.
         for item in query_db('select * from STUDENT NATURAL JOIN GRADES'):
             students.append(item)
         db.close()
-
-        return render_template('iviewgrades.html', studentH=students)        
-    else:
-        request.form['grade']
-        request.form['examname']
-        request.form['utorid']
+        
+        return render_template('iviewgrades.html', studentH=students)
+    else:  # Work in Progress - For Instructor to enter marks
+        # request.form['grade']
+        # request.form['examname']
+        # request.form['utorid']
         try:
             db = get_db()
-            db.execute('UPDATE GRADES SET ?=? WHERE UTORID=?', examname, grade, utorid)
+            db.execute('UPDATE GRADES SET ?=? WHERE UTORID=?', [examname], [grade], [utorid])
             db.close()
 
             return render_template('iviewgrades.html')
         except db.Error as err:
-            return render_template('error_redirect.html', itemHTML=err)
+            return redirect(redirect_url())
         finally:
             db.close()
 
 # Instructor views feedback, removes feedback
 @app.route('/iviewfeedback.html', methods=['GET', 'POST'])
 def instructor_view_feedback():
+    if session['user_type'] != "instructor"        
+        return redirect(redirect_url())
     db = get_db()
     # Each row from the table is placed in dictionary form
     db.row_factory = make_dicts
-
     feedback = []
-    # Inside DB are some tables.
-    for item in query_db('select FA, FB, FC, FD from FEEDBACK'):
+    # Sample username from session (To be implemented)
+    session['username'] = "instructor1"
+    for item in query_db('select FA, FB, FC, FD from FEEDBACK NATURAL JOIN USER WHERE USERNAME LIKE ?', [session['username']]):
         feedback.append(item)
-
     db.close()
 
     # return fb.__str__()
-    return render_template('iviewfeedback.html', feedbackH = feedback)
+    return render_template('iviewfeedback.html', feedbackH=feedback)
 
 # Instructor views remark requests, removes remark requests
 @app.route('/iviewremarks.html', methods=['GET', 'POST'])
 def instructor_view_remarks():
+    if session['user_type'] != "instructor"        
+        return redirect(redirect_url())
     db = get_db()
     # Each row from the table is placed in dictionary form
     db.row_factory = make_dicts
-
     remarks = []
     # Inside DB are some tables.
     for item in query_db('select UTORID, FNAME, LNAME, EXAMNAME, COMMENT FROM REMARKS NATURAL JOIN STUDENT'):
         remarks.append(item)
-
     db.close()
-
-    # return remark.__str__()
     return render_template('iviewremarks.html', remarksH=remarks)
+
+# Redirect back to the previous page (if the user attempts to access something bad)
+# From https://flask.palletsprojects.com/en/1.1.x/reqcontext/
+def redirect_url(default='index.html'):
+    return request.args.get('next') or request.referrer or url_for(default)
+    # Use >> return redirect(redirect_url()) << In function call
 
 # Landing page. What will it be?
 @app.route('/')
@@ -177,7 +176,9 @@ def lectures_page():
 
 @app.route('/links.html')
 def links_page():
-    return render_template('links.html')
+    # Testing - Instructor View (Remove later)
+    session['user_type'] = "instructor"
+    return render_template('links.html', user_type=session['user_type'])
 
 @app.route('/team.html')
 def team_page():
@@ -193,5 +194,5 @@ def tutorials_page():
 
 # -------------------------------------------- Port --------------------------------------
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
