a60abad7180d94c0be10452a531c5c025c472faf
JL <52089352+TheJohnsonLai@users.noreply.github.com>
2021-03-24 13:59:20 -0400

    Major CSS changes, .html re-added to work offline

--- a3/static/js/mobile.js
+++ a3/static/js/mobile.js
@@ -1,9 +1,13 @@
 // Flips nav-bar ON and OFF, mobile view
 function flipMenu() {
     var m = document.getElementById("nav-menu");
-    if (m.style.display === "block") {
+    var h = document.getElementById("hidden-header");
+    // Style can start as NULL
+    if (m.style.display != "none") {
         m.style.display = "none";
+        h.style.display = "block";
     } else {
         m.style.display = "block";
+        h.style.display = "none";
     }
 }
